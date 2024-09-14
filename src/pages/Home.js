00a2b697971e1f6aa5e4f3aa6be8f1e1a40cf06a
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-09-14 22:28:42 +0800

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="w-full text-center">Welcome To Naz Laboratory</div>
+    </div>
+  );
+}
+
+export default Home;
